855437249d8e1fb9270eca29bd6d47fc9cd91c07
ApxuTechTop <apxutech@gmail.com>
2026-02-10 22:24:58 +0700

    Change storage node

--- app/app.ts
+++ app/app.ts
@@ -1,4 +1,4 @@
 namespace $ {
-	export let $apxu_samosbor_map_app_storage_ref = () => "QmLw5lJT_3uA1hzjn"
-	export let $apxu_samosbor_map_app_my_public_key = () => "_-lCYvDmUlPe4DWHOOeJ7BFeIEJuI-lHCJept_u19SAUWGIg-uRgUgcxEHw-wSCL6deScK98QcwnFBEKVKpD-Q"
+	export let $apxu_samosbor_map_app_storage_ref = () => "g3UoIWgL_ZrGrGjME"
+	export let $apxu_samosbor_map_app_my_public_key = () => "_0A4kOtpHW-gbXdF2_cdgMkDjQt0UVXE56EDsymP3X0icg7srBBAQSUCrvX2nY8uMGpoNDAF6KhDhPjSm2SYFY"
 }
