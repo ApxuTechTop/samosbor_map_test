02425543c9ad6b95b093c63f01474ed46442820e
ApxuTechTop <apxutech@gmail.com>
2026-02-10 23:05:59 +0700

    change storage

--- app/app.ts
+++ app/app.ts
@@ -1,4 +1,4 @@
 namespace $ {
-	export let $apxu_samosbor_map_app_storage_ref = () => "g3UoIWgL_ZrGrGjME"
-	export let $apxu_samosbor_map_app_my_public_key = () => "_0A4kOtpHW-gbXdF2_cdgMkDjQt0UVXE56EDsymP3X0icg7srBBAQSUCrvX2nY8uMGpoNDAF6KhDhPjSm2SYFY"
+	export let $apxu_samosbor_map_app_storage_ref = () => "3H1JR67L_eOUlz9Km"
+	export let $apxu_samosbor_map_app_my_public_key = () => "_7Bx9ojuIY7hg7PoYPvIkctqdNeUrLmTh5JAotKkmgQTcJCdI5SAMGUjR6q0blm1gsP_noMbPYIyZKHVhoOcvw"
 }
